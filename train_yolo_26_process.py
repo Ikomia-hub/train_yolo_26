088ee445041765959c3b777fc4612b6367e56250
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-24 11:21:46 +0100

    chore: Qt 6

--- train_yolo_26_process.py
+++ train_yolo_26_process.py
@@ -167,7 +167,7 @@ class TrainYolo26Factory(dataprocess.CTaskFactory):
         self.info.short_description = "Train YOLO26 object detection models."
         self.info.path = "Plugins/Python/Detection"
         self.info.version = "1.0.0"
-        self.info.min_ikomia_version = "0.15.0"
+        self.info.min_ikomia_version = "0.16.0"
         self.info.icon_path = "images/icon.png"
         self.info.authors = "Jocher, G., Chaurasia, A., & Qiu, J"
         self.info.article = "YOLO by Ultralytics"
--- train_yolo_26_widget.py
+++ train_yolo_26_widget.py
@@ -1,9 +1,10 @@
+# PyQt GUI framework
+from PyQt6.QtWidgets import *
+
 from ikomia import core, dataprocess
 from ikomia.utils import pyqtutils, qtconversion
-from train_yolo_26.train_yolo_26_process import TrainYolo26Param
 
-# PyQt GUI framework
-from PyQt5.QtWidgets import *
+from train_yolo_26.train_yolo_26_process import TrainYolo26Param
 
 
 class TrainYolo26Widget(core.CWorkflowTaskWidget):
@@ -31,14 +32,13 @@ class TrainYolo26Widget(core.CWorkflowTaskWidget):
             self.grid_layout, label="Dataset folder",
             path=self.parameters.cfg["dataset_folder"],
             tooltip="Select folder",
-            mode=QFileDialog.Directory
+            mode=QFileDialog.FileMode.Directory
         )
 
         self.spin_epochs = pyqtutils.append_spin(
             self.grid_layout, "Epochs", self.parameters.cfg["epochs"])
 
-        self.spin_batch = pyqtutils.append_spin(
-            self.grid_layout, "Batch size", self.parameters.cfg["batch_size"])
+        self.spin_batch = pyqtutils.append_spin(self.grid_layout, "Batch size", self.parameters.cfg["batch_size"])
 
         self.spin_train_test_split = pyqtutils.append_double_spin(
             self.grid_layout,
@@ -51,14 +51,13 @@ class TrainYolo26Widget(core.CWorkflowTaskWidget):
         custom_hyp = bool(self.parameters.cfg["config_file"])
         self.check_hyp = QCheckBox("Custom hyper-parameters")
         self.check_hyp.setChecked(custom_hyp)
-        self.grid_layout.addWidget(
-            self.check_hyp, self.grid_layout.rowCount(), 0, 1, 2)
+        self.grid_layout.addWidget(self.check_hyp, self.grid_layout.rowCount(), 0, 1, 2)
         self.check_hyp.stateChanged.connect(self.on_custom_hyp_changed)
 
         self.label_hyp = QLabel("Hyper-parameters file")
         self.browse_hyp_file = pyqtutils.BrowseFileWidget(path=self.parameters.cfg["config_file"],
                                                           tooltip="Select file",
-                                                          mode=QFileDialog.ExistingFile)
+                                                          mode=QFileDialog.FileMode.ExistingFile)
 
         row = self.grid_layout.rowCount()
         self.grid_layout.addWidget(self.label_hyp, row, 0)
@@ -71,7 +70,7 @@ class TrainYolo26Widget(core.CWorkflowTaskWidget):
             self.grid_layout, label="Output folder",
             path=self.parameters.cfg["output_folder"],
             tooltip="Select folder",
-            mode=QFileDialog.Directory
+            mode=QFileDialog.FileMode.Directory
         )
 
         layout_ptr = qtconversion.PyQtToQt(self.grid_layout)
@@ -86,8 +85,7 @@ class TrainYolo26Widget(core.CWorkflowTaskWidget):
         self.parameters.cfg["dataset_folder"] = self.browse_dataset_folder.path
         self.parameters.cfg["epochs"] = self.spin_epochs.value()
         self.parameters.cfg["batch_size"] = self.spin_batch.value()
-        self.parameters.cfg["dataset_split_ratio"] = self.spin_train_test_split.value(
-        )
+        self.parameters.cfg["dataset_split_ratio"] = self.spin_train_test_split.value()
         self.parameters.cfg["output_folder"] = self.browse_out_folder.path
         if self.check_hyp.isChecked():
             self.parameters.cfg["config_file"] = self.browse_hyp_file.path
